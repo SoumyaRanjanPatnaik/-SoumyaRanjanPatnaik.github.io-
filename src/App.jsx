cb81633208a46417681399fef7055e71b7f24026
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-02-17 05:31:16 +0530

    Fastrack

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,55 @@
 import "./App.css";
 import Navbar from "./Nav/Nav";
-import { HomeDark } from "./Home/Home";
+import {HomeDark} from "./Home/Home";
 import About from "./About/About";
 import Resume from "./Resume/Resume";
-import { useLocation } from "react-router-dom";
-import links from "./Nav/links";
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import windowContext from "./windowContext";
+import {Route, Routes} from "react-router";
 
 function App() {
-  const currUrl = useLocation();
-  const [windowDim, setWindowDim] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  //const [currNavIndex, setCurrNavIndex] = useState(0);
-  const [homeRef, aboutRef, resumeRef] = [
-    useRef(null),
-    useRef(null),
-    useRef(null),
-  ];
-  const handleResize = () => {
-    setWindowDim({ width: window.innerWidth, height: window.innerHeight });
-  };
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
+    const [windowDim, setWindowDim] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+    //const [currNavIndex, setCurrNavIndex] = useState(0);
+    const [homeRef, aboutRef, resumeRef] = [
+        useRef(null),
+        useRef(null),
+        useRef(null),
+    ];
+    const handleResize = () => {
+        setWindowDim({width: window.innerWidth, height: window.innerHeight});
     };
-  }, []);
-  useEffect(() => {
-      const curr_id = links.find((item) => item.url === currUrl.pathname)?.id;
-      const elementDOM = document.getElementById(curr_id);
-      if (elementDOM) {
-        elementDOM.scrollIntoView({
-          behavior: "smooth",
-          block: "center",
-        });
-      }
-  }, [currUrl, windowDim]);
-  return (
-    <windowContext.Provider value={windowDim}>
-      <div
-        className={`h-screen overflow-y-hidden overflow-x-hidden scrollbar scrollbar-thin scrollbar-track-transparent  scrollbar-thumb-gray-400 ${
-            windowDim.height >= 500 ? "snapY sm:overflow-y-hidden":"overscroll-y-auto"
-        }`}
-        dir="ltr"
-      >
-        <Navbar />
-        <HomeDark refContainer={homeRef} />
-        <About refContainer={aboutRef} />
-        <Resume refContainer={resumeRef} />
-      </div>
-    </windowContext.Provider>
-  );
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    return (
+        <windowContext.Provider value={windowDim}>
+            <div
+                className={`h-screen overflow-y-hidden overflow-x-hidden scrollbar scrollbar-thin scrollbar-track-transparent  scrollbar-thumb-gray-400 ${windowDim.height >= 500 ? "snapY sm:overflow-y-hidden" : "overscroll-y-auto"
+                    }`}
+                dir="ltr"
+            >
+                <Navbar />
+
+                <Routes>
+                    <Route path="/" element={
+                        <HomeDark refContainer={homeRef} />
+                    } />
+                    <Route path="/about" element={
+                        <About refContainer={aboutRef} />
+                    } />
+                    <Route path="/resume" element={
+                        <Resume refContainer={resumeRef} />
+                    } />
+                </Routes>
+            </div>
+        </windowContext.Provider>
+    );
 }
 
 export default App;
