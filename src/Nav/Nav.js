8ef2edb8cee6b14cce086e39a7ca629d791bef26
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-02-20 07:20:07 +0530

    Fixed minor issues with navbar, optimised transition, fixed dark text for scrollDown button

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,106 +1,110 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, {useState, useRef, useEffect} from "react";
 import Icon from "../Icon/Icon";
-import { FiMenu } from "react-icons/fi";
+import {FiMenu} from "react-icons/fi";
 import links from "./links";
 import SingleLink from "./SingleLink";
-import { Link, useLocation } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
-  const [isShow, setIsShow] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(Number(window.innerWidth));
-  const linksContainerRef = useRef(null);
-  const linksRef = useRef(null);
-  useEffect(() => {
+    const [isShow, setIsShow] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(Number(window.innerWidth));
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
     const handleResize = () => {
-      setWindowWidth((prev) => {
-        const curr = Number(window.innerWidth);
-        if (windowWidth >= 640 && curr < 640) {
-          setIsShow(true);
-        } else {
-          setIsShow(false);
+        setWindowWidth((prev) => {
+            const curr = Number(window.innerWidth);
+            if (windowWidth >= 640)
+                setIsShow(true)
+            else
+                setIsShow(false)
+            return curr
+        });
+    };
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    });
+
+    const closeMenu = () => {
+        if (windowWidth < 640)
+            setIsShow(false);
+    };
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+        if (windowWidth >= 640) {
+            setIsShow(true);
+            linksContainerRef.current.style.height = "fit-content";
+            linksContainerRef.current.style.transition = "none"
         }
-      });
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, windowWidth);
+        else {
+            if (isShow) {
+                linksContainerRef.current.style.height = `${linksHeight}px`;
+            } else {
+                linksContainerRef.current.style.height = "0px";
+            }
+        }
+    }, [isShow, windowWidth]);
 
-  const closeMenu = () => {
-    setIsShow(false);
-  };
-
-  useEffect(() => {
-    const linksHeight = linksRef.current.getBoundingClientRect().height;
-
-    if (windowWidth >= 640) {
-      linksContainerRef.current.style.height = "3rem";
-    } else {
-      if (isShow) {
-        linksContainerRef.current.style.height = `${linksHeight}px`;
-      } else {
-        linksContainerRef.current.style.height = "0px";
-      }
-    }
-  }, [isShow, windowWidth]);
-
-  return (
-    <header className="fixed top-0 z-50 w-full min-h-12">
-      <nav
-        className="relative px-2 py-2 sm:py-3 md:px-3 lg:px-4  flex flex-col 
-      text-white bg-gray-900 sm:flex-row sm:justify-between overflow-hidden"
-      >
-        {/*Nav Header */}
-        <div className="flex items-center justify-between w-full h-12 my-auto">
-          <Link to="/">
-            <Icon />
-          </Link>
-          <div
-            className=" absolute right-3  sm:translate-y-72 flex items-center 
-            cursor-pointer transition transform  duration-300 hover:scale-110 "
-            onClick={() => {
-              setIsShow(!isShow);
-            }}
-          >
-            <span className="text-gray-100 font-semibold">Navigate</span>
-            <FiMenu
-              className={`m-2 text-xl sm:hidden transition-transform
-              transform  duration-300 ${isShow ? "rotate-90" : ""}`}
-            />
-          </div>
-        </div>
-        {/* Nav Links Container*/}
-        <div
-          className="w-full h-full overflow-hidden sm:w-auto 
-                    sm:overflow-visible transition-all sm:translate-y-7 duration-[250]"
-          ref={linksContainerRef}
-        >
-          {/* Links */}
-          <ul
-            className="flex flex-col text-center 
-                        items-stretch w-full sm:flex-row sm:h-full
-                        sm:mr-3 justify-center items"
-            ref={linksRef}
-          >
-            {links.map((item, index) => {
-              return (
-                <SingleLink
-                  className=""
-                  path={item.url}
-                  name={item.name}
-                  id={item.id}
-                  disabled={item.disbled}
-                  key={index}
-                  closeMenu={closeMenu}
-                />
-              );
-            })}
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="fixed top-0 z-50 w-full hit-fit">
+            <nav
+                className="relative px-2 lg:px-4 flex flex-col h-fit 
+                text-white bg-gray-900 sm:flex-row sm:justify-between overflow-hidden"
+            >
+                {/*Nav Header */}
+                <div className="flex items-center justify-between w-full h-16 xl:h-20 my-auto">
+                    <Link to="/">
+                        <Icon />
+                    </Link>
+                    <div
+                        className=" absolute right-3  sm:translate-y-72 flex items-center 
+                        cursor-pointer transition transform  duration-300 hover:scale-110 "
+                        onClick={() => {
+                            setIsShow(!isShow);
+                        }}
+                    >
+                        <span className="text-gray-100 font-semibold">Navigate</span>
+                        <FiMenu
+                            className={`m-2 text-xl sm:hidden transition-transform transform 
+                            duration-300 ${isShow && "rotate-90"}`}
+                        />
+                    </div>
+                </div>
+                {/* Nav Links Container*/}
+                <div
+                    className={`flex items-center w-full h-full overflow-hidden sm:w-auto 
+                    sm:overflow-visible transition-all duration-[250]`}
+                    ref={linksContainerRef}
+                >
+                    {/* Links */}
+                    <ul
+                        className={`flex flex-col text-center items-stretch w-full duration-300
+                        sm:flex-row sm:h-full ${isShow ? "translate-y-1 xl:translate-y-4" : "-translate-y-7 opacity-0"} 
+                        sm:mr-3 justify-center items`}
+                        ref={linksRef}
+                    >
+                        {links.map((item, index) => {
+                            return (
+                                <SingleLink
+                                    className=""
+                                    path={item.url}
+                                    name={item.name}
+                                    id={item.id}
+                                    disabled={item.disbled}
+                                    key={index}
+                                    closeMenu={closeMenu}
+                                />
+                            );
+                        })}
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
--- src/Nav/SingleLink.js
+++ src/Nav/SingleLink.js
@@ -1,29 +1,27 @@
 import React from "react";
-import { useLocation, NavLink } from "react-router-dom";
+import {useLocation, NavLink} from "react-router-dom";
 
-const SingleLink = ({ path, name, closeMenu, id }) => {
-  const { pathname } = useLocation();
-  return (
-    <li className="relative flex-1 flex flex-col justify-center list-none gap-2 group ">
-      <NavLink
-        to={path}
-        className={`flex w-full sm:w-max sm:p-1 ${
-          pathname === path && "text-green-100"
-        } rounded-lg  h-8 sm:h-12 items-center px-1 transition-all`}
-        onClick={closeMenu}
-      >
-        {name}
-      </NavLink>
-      <div
-        className={`hidden sm:block h-0 origin-left border-green-100 
+const SingleLink = ({path, name, closeMenu, id}) => {
+    const {pathname} = useLocation();
+    return (
+        <li className="relative flex-1 flex flex-col justify-center list-none gap-1 group ">
+            <NavLink
+                to={path}
+                className={`flex w-full sm:w-max sm:p-1 ${pathname === path && "text-green-100"
+                    } rounded-lg  h-10 sm:h-12 items-center px-1 transition-all`}
+                onClick={closeMenu}
+            >
+                {name}
+            </NavLink>
+            <div
+                className={`hidden sm:block h-0 origin-left border-green-100 
                     border-2 w-5/6 relative bottom sm:bottom-3.5 left-1.5
-                    ${
-                      pathname === path ? "scale-x-30" : "scale-x-0"
+                    ${pathname === path ? "scale-x-30" : "scale-x-0"
                     } rounded-md 
                 group-hover:scale-x-100 transform transition-transform `}
-      />
-    </li>
-  );
+            />
+        </li>
+    );
 };
 
 export default SingleLink;
